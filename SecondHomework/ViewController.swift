0ee7c43b6a567e93b725f7cdfaae9c1b8f52d38d
breetva <breetva@gmail.com>
2022-06-18 13:42:47 +0200

    Initial Commit

---
+++ SecondHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondHomework
+//
+//  Created by Dmitry Pavlov on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
